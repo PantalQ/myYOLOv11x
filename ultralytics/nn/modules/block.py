fb50a990c14859418cef37f9e3f2975bb7925173
PantalQ <gf20091ban16@mail.dlut.edu.cn>
2025-05-12 10:36:11 +0800

    add import in bloack.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from ultralytics.utils.torch_utils import fuse_conv_and_bn
+from ultralytics.nn.AddModules import SimAM
 
 from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, autopad
 from .transformer import TransformerBlock
